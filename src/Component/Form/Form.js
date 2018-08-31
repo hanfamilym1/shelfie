5442c1962d4ea2af7ada8085112b2e53e2b57237
hanfamilym1 <michael@hanfamily.com>
2018-08-31 11:48:05 -0600

    TS: 11:47 Finished Step 1

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -4,12 +4,55 @@ class Form extends Component {
     constructor(props){
         super(props)
         this.state={
-
+            urlinput: '',
+            productinput: '',
+            price: 0
         }
+        this.handleUrlInput = this.handleUrlInput.bind(this)
+        this.resetButton = this.resetButton.bind(this)
     }
+
+    handleUrlInput(val){
+        this.setState({
+            urlinput: val
+        })
+    }
+    handleProductInput(val){
+        this.setState({
+            productinput: val
+        })
+    }
+
+    handlePrice(val){
+        this.setState({
+            price: val
+        })
+    }
+
+    resetButton(){
+        this.setState({
+            urlinput: '',
+            productinput: '',
+            price: 0
+        })
+    }
+
     render(){
+        console.log(this.state.urlinput)
+        console.log(this.state.productinput)
+        console.log(this.state.price)
         return(
-            <div>Form Component</div>
+            <div className='form'>
+                <label htmlFor="">Image URL</label> <br/>
+                <input type="text" value={this.state.urlinput} onChange={(e=>this.handleUrlInput(e.target.value))}/> <br/>
+                <label htmlFor="">Product Name</label> <br/>
+                <input type="text" value={this.state.productinput} onChange={(e=>this.handleProductInput(e.target.value))}/> <br/>
+                <label htmlFor="">Price</label> <br/>
+                <input type="number" value={this.state.price} onChange={(e=>this.handlePrice(e.target.value))}/> <br/>
+                <button onClick={this.resetButton}>Cancel</button>
+                <button>Add to Inventory</button>
+
+            </div>
         )
     }
 }
